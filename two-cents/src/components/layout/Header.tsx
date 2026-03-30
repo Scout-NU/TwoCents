22f35836397990c705ea9dce6478403cf1a11d28
[madisonseal1] <madisonseal@icloud.com>
2026-03-30 17:44:24 -0400

    feat/header made responsive

--- two-cents/src/components/layout/Header.tsx
+++ two-cents/src/components/layout/Header.tsx
@@ -1,26 +1,68 @@
 import {useLocation} from "react-router-dom";
+import { useState } from "react";
+
 
 const Header = () => {
-
+    const [open, setOpen] = useState(false);
     const location = useLocation();
+    const navLinks = [
+    {href: "/home", label: "Home"} ,
+    {href: "/about", label: "About"},
+    {href: "/pricing", label: "Pricing"}];
     
 
     return (
-        <header className=" flex justify-between items-center gap-2 bg-white px-7 py-6 border-black border ">
+        
+        <header className="relative flex justify-between items-center gap-2  border bg-white px-7 py-6">
+            {/* Logo */}
             <div className="relative">
-            <div className=" whitespace-nowrap mr-8 text-2xl">
-                <span className="text-[#FA9E4D]"> Two  </span><span className="text-[#52AEEF]"> Cents </span>
+                <div className=" whitespace-nowrap mr-8 text-2xl">
+                    <span className="text-[#FA9E4D]"> Two  </span><span className="text-[#52AEEF]"> Cents </span>
             </div>
             <img  className="w-12 absolute top-1 right-19" src="/images/centy-bill.png"></img>
             </div>
-            <nav className="flex gap-10 items-center font-semibold">
-                <a href="/home" className={location.pathname === "/home" ? "underline decoration-[0.75px] underline-offset-2" : ""}>Home</a>
-                <a href="/about"  className={location.pathname === "/about" ? "underline decoration-[0.75px] underline-offset-2" : ""}>About</a>
-                <a href="/pricing"  className= {location.pathname === "/pricing" ? "underline decoration-[0.75px] underline-offset-2" : ""}>Pricing</a>
+
+            {/* Desktop NavBar */}
+            <nav className="items-center font-semibold hidden md:flex" >
+            <div className="flex gap-10">
+                {navLinks.map(({label, href}) => (
+                    <a
+                        key={label} 
+                        href={href} 
+                        className={location.pathname === href ? "underline decoration-[0.75px] underline-offset-2" : ""}
+                        >
+                        {label}
+                    </a>
+                ))}
+                
                 <a className="text-[#FA9E4D] bg-white border border-[#FA9E4D] px-2 rounded-xl pr-4 whitespace-nowrap hover:bg-[#FA9E4D] hover:text-white transition ease-in-out" href="/join-waitlist" >Join Waitlist</a>
+            </div>
             </nav>
-        </header>
+            {/* Hamburger */}
+            <button onClick={() => setOpen(!open)}
+            className=" md:hidden flex-col justify-center items-center gap-1.5 w-10 h-8 text-3xl">
+            ☰</button>
+
+            <div className={`fixed top-0 right-0 h-full w-2/3 bg-white transform transition-transform duration-300 ${open ? "translate-x-0" : "translate-x-full"}`}>
+            <button onClick={() => setOpen(false)} className={`absolute top-5 right-5 text-2xl transform transition-transform duration-300 ${open ? "" : "hidden"}`}>✕</button>
+
+            {/* Mobile Dropdown */}
+            <nav className=" flex flex-col mt-20 items-center gap-8 text-lg font-semibold">
+            {navLinks.map(({label, href}) => (
+                <a
+                    key={label}
+                    href={href}
+                    onClick={() => setOpen(false)}
+                    className={location.pathname === href ? "underline decoration-[0.75px] underline-offset-2" : ""}
+                    >{label}</a>
+            ))}
+            <a 
+            className= {` ${location.pathname === '/join-waitlist' ? "bg-[#FA9E4D] text-white" : ""} $bg-white text-[#FA9E4D]  border border-[#FA9E4D] px-2 rounded-xl pr-4 whitespace-nowrap hover:bg-[#FA9E4D] hover:text-white transition ease-in-out`}
+            href="/join-waitlist" >Join Waitlist</a>
+            </nav>
+        </div>
+    </header>
     )
 }
 
-export default Header
+export default Header;
