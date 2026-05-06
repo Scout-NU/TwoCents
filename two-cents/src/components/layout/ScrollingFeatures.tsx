fd3c5994bf4e61bdfbf2e1e0e64292879041f1b0
Jolin Yang <yang.jol@northeastern.edu>
2026-05-05 21:21:10 -0400

    feat: make features display static

--- two-cents/src/components/layout/ScrollingFeatures.tsx
+++ two-cents/src/components/layout/ScrollingFeatures.tsx
@@ -1,15 +1,15 @@
 function ScrollingFeatures() {
 
     return (
-        <div className="max-w-4xl mx-auto overflow-hidden">
-            <div className="flex gap-4 w-max animate-scrollCarousel">
-                <img src="images/feature 1.png" alt="proactive spending feature" className="w-40 md:w-65 shrink-0" loading="eager"/>
+        <div className="max-w-4xl mx-auto">
+            <div className="flex gap-4 w-max">
+            <img src="images/feature 1.png" alt="proactive spending feature" className="w-45 md:w-70"/>
+                <img src="images/feature 2.png" alt="priorities (not budgets) feature" className="w-45 md:w-70"/>
+                <img src="images/feature 3.png" alt="real-time bank sync feature"  className="w-45 md:w-70"/>
+                
+                {/* <img src="images/feature 1.png" alt="proactive spending feature" className="w-40 md:w-65 shrink-0" loading="eager"/>
                 <img src="images/feature 2.png" alt="priorities (not budgets) feature" className="w-40 md:w-65 shrink-0"/>
-                <img src="images/feature 3.png" alt="real-time bank sync feature"  className="w-40 md:w-65 shrink-0"/>
-
-                <img src="images/feature 1.png" alt="proactive spending feature" className="w-40 md:w-65 shrink-0" loading="eager"/>
-                <img src="images/feature 2.png" alt="priorities (not budgets) feature" className="w-40 md:w-65 shrink-0"/>
-                <img src="images/feature 3.png" alt="real-time bank sync feature"  className="w-40 md:w-65 shrink-0"/>
+                <img src="images/feature 3.png" alt="real-time bank sync feature"  className="w-40 md:w-65 shrink-0"/> */}
             </div>
         </div>
     );
